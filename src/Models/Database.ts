d8af0a1538b8a66ee86d55530084f1df3fa6c836
Mark Goltsman <mmgoltsman@mail.ru>
2024-11-14 23:04:10 -0500

    Implemented error handling and proper status code responses for database class

--- src/Models/Database.ts
+++ src/Models/Database.ts
@@ -3,6 +3,25 @@ import {v4 as uuid, validate} from "uuid";
 
 
 export type UUID = string;
+export type statusCodes = 200 | 201 | 204 | 400 | 404
+
+export class DatabaseError extends Error {
+    private code: statusCodes;
+
+    public static errors: {[key in statusCodes] : string } = {
+        200: "Successfully retrieved data",
+        201: "Successfully created entry",
+        204: "Successfully deleted data",
+        400: "Invalid input. Request failed",
+        404: "Data not found"
+    }
+
+    constructor(code: statusCodes, message: string = DatabaseError.errors[code]) {
+        super(message);
+        this.code = code;
+    }
+}
+
 
 export default class Database <Type> {
     protected data: { [key: UUID]: Type };
@@ -13,48 +32,48 @@ export default class Database <Type> {
 
     public read(entryId: UUID) {
         if (!validate(entryId)) {
-            console.log(400); // 400
+            throw new DatabaseError(400);
         }
         try {
             let entry: Type = this.data[entryId];
-            return entry;
+            return {status: 200, message: entry};
         } catch (e) {
-            console.log(e); // 404
+            throw new DatabaseError(404);
         }
     }
 
     public readAll() {
-        return this.data;
+        return {status: 200, message: this.data};
     }
 
     public create(entry: Type) {
         if(!Object.keys(this.data).every((key: UUID) => this.data[key])) {
-            return "-1"; // 400
+            throw new DatabaseError(400);
         };
         let id: UUID = uuid();
         this.data[id] = entry;
-        return id; // 201
+        return {status: 201, message: id}; // 201
     }
 
     public update(entryId: UUID, entry: Type)  {
         if(!validate(entryId)) {
-            return false; //400
+            throw new DatabaseError(400);
         }
         if(!this.read(entryId)) {
-            return false; // 404
+            throw new DatabaseError(404);
         }
         this.data[entryId] = entry;
-        return this.data[entryId]; // 200
+        return {status: 200, message: this.data[entryId]};
     }
 
     public delete(entryId: UUID) {
         if(!validate(entryId)) {
-            return false; // 400
+            throw new DatabaseError(400);
         }
         if(!this.read(entryId)) {
-            return false; // 404
+            throw new DatabaseError(400);
         }
         delete this.data[entryId];
-        return true; // 204
+        return {status: 204, message: DatabaseError.errors[204]}; // 204
     }
 }
